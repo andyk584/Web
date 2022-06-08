69b48258fd49e8dc40ac8fed08d7cf5f3b30c449
andyk584 <105589739+andyk584@users.noreply.github.com>
2022-06-08 17:10:26 +0800

    edit

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-// var form = document.querySelector("myform");
-// const myHeading = document.querySelector('h1');
-// myHeading.textContent = 'Hello world!';
 let nominal = 0;
 let real = 0;
 let remain = 0;
 
-//document.getElementById("btn").addEventListener("click", cal);
-
 function cal() {
-  const start = document.getElementById("start").value;
-  const deduct = document.getElementById("deduct").value;
-  const remand = document.getElementById("remand").value;
+  const start = Number(document.getElementById("start").value);
+  const deduct = Number(document.getElementById("deduct").value);
+  const remand = Number(document.getElementById("remand").value);
   const plea = document.querySelector("input[name='plea']:checked").value;
 /*   const pleaEle = document.getElementsByName("plea");  
   for (let i = 0; i < pleaEle.length; i++) {
@@ -28,16 +23,24 @@ function cal() {
   real = nominal * 2 / 3 - remand;
   remain = real - remand;
   
+  if (start <= deduct) {
+	alert("The starting point must be greater than the deduction.");
+	nominal = 0;
+	real = 0;
+	remain = 0;
+  }
+  
   if (remand > nominal) {
 	real = remand;
-	remain = 0;
-  }　else if (remain < 1) {
+    remain = 0;
+  } else if (remain < 1) {
 	real = remand + 1;
 	remain = 1;
   }
   
   return nominal, real, remain;	
 }
+
 nominal = cal()[0];
 real = cal()[1];
 remain = cal()[2];
